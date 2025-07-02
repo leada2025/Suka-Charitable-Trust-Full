3cc0e55e5c2f0bcbd8a86e44c2b16564f7aa6084
leada2025 <webdevldd@gmail.com>
2025-07-02 12:09:18 +0530

    model

--- backend/models/User.js
+++ backend/models/User.js
@@ -15,7 +15,6 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    
     password: {
       type: String,
       required: true,
@@ -25,13 +24,38 @@ const userSchema = new mongoose.Schema(
       enum: ['Admin', 'Volunteer', 'Donor'],
       default: 'Volunteer',
     },
-    resetToken: String,
-resetTokenExpiry: Date,
 
+    // Newly Added Fields
+    bloodGroup: {
+      type: String,
+      enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+    },
+    address: {
+      type: String,
+    },
+    reasonForJoining: {
+      type: String,
+    },
+    areaOfInterest: {
+      type: String,
+    },
+    gender: {
+      type: String,
+      enum: ['Male', 'Female', 'Other'],
+    },
+    age: {
+      type: Number,
+      min: 0,
+    },
+    dateOfBirth: {
+      type: Date,
+    },
+
+    // For password reset
+    resetToken: String,
+    resetTokenExpiry: Date,
   },
   { timestamps: true }
-
-
 );
 
 module.exports = mongoose.model('User', userSchema);

5c1649a40c1920af52a100e7c8c3342b0c94eee0
leada2025 <webdevldd@gmail.com>
2025-07-02 11:06:06 +0530

    vite confiq

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // if you're using React
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  base: './',
+  base: '/dashboard/', // ✅ this must match your Render subpath
   plugins: [
+    react(),            // ✅ optional but common for React apps
     tailwindcss(),
   ],
 })

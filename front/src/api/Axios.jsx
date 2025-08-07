fd97783ce98e6c2a8b00ff0c837c25a1fde188a1
leada2025 <webdevldd@gmail.com>
2025-08-07 11:24:13 +0530

    ma, photo updated

--- front/src/api/Axios.jsx
+++ front/src/api/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://suka-charitable-trust-full-backend.onrender.com',
   withCredentials: true, // if you're using cookies for auth
 });
 
